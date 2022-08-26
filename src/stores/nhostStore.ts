050cb982073a94f73febfdfc43607e580c8cf9d2
CJ Lazell <cjlazell@gmail.com>
2022-08-25 20:08:25 -0500

    log nhostStore variables

--- src/stores/nhostStore.ts
+++ src/stores/nhostStore.ts
@@ -1,6 +1,9 @@
 import { NhostClient } from '@nhost/nextjs'
 import { atom } from 'nanostores'
 
+console.log('PUBLIC_NHOST_SUBDOMAIN', import.meta.env.PUBLIC_NHOST_SUBDOMAIN)
+console.log('PUBLIC_NHOST_REGION', import.meta.env.PUBLIC_NHOST_REGION)
+
 const nhostClient = new NhostClient({
   subdomain: import.meta.env.PUBLIC_NHOST_SUBDOMAIN,
   region: import.meta.env.PUBLIC_NHOST_REGION,
